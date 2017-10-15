7524ab659258f31d57381b7449e43dc46f290f2b
Chen-yu Li <qwe153246@gmail.com>
2017-10-15 13:16:24 +0800

    V1.0

--- stamp.js
+++ stamp.js
@@ -1,45 +1,55 @@
 var S = module.exports = {}
 
-S.sumUpStamps = function(stamps, ia) {
-    let p = stamps;
+S.sumUpStamps = function (stamps, ia) {
+    let [...p] = stamps;
     let sum = 0;
-    ia.forEach(function(element) {
+    ia.forEach(function (element) {
         sum += p[element];
-    }, p);
+    });
     return sum;
 }
 
-S.initIA = function(stamps, desired) {
+S.initIA = function (stamps, desired) {
     if (stamps.length == 0) return undefined;
-    let p = stamps;
+    let [...p] = stamps;
     let b = p[p.length - 1];
-    let a = []; 
-    for(let n = 0; desired > b * n; n++)
-    {
-            a.push(p.length - 1);
+    let a = [];
+    for (let n = 0; desired > b * n; n++) {
+        a.push(p.length - 1);
     }
     return a;
 }
 
-S.nextIAstepDown = function(stamps, ia, desired) {
-    //TODO
+S.nextIAstepDown = function (stamps, ia, desired) {
+    let [...price] = stamps;
+    let [...index] = ia;
+    let target = desired;
+    let temp = 0;
+    while (S.sumUpStamps(price, index) > target) {
+        temp = index.pop();
+    }
+    while (S.sumUpStamps(price, index) < target) {
+        temp = temp - 1;
+        if (temp < 0) temp = 0;
+        index.push(temp);
+    }
+    return index;
 }
 
-S.findStamp = function(stamps, desired) {
-    stamps.sort()
-    var ia = S.initIA(stamps, desired)
-
-    while (S.sumUpStamps(stamps, ia) > desired) {
-        ia = S.nextIAstepDown(stamp, ia, desired)
+S.findStamp = function (stamps, desired) {
+    let [...price] = stamps;
+    price.sort();
+    let [...index] = S.initIA(price, desired);
+    let target = desired;
+    let result = [];
+    let temp = [];
+    while (S.sumUpStamps(price, index) > target) {
+        [...temp] = S.nextIAstepDown(price, index, target);
+        if (index[index.length - 1] == temp[temp.length - 1]) break;
+        [...index] = temp;
     }
-
-    if(S.sumUpStamps(stamps, ia) !== desired) {
-        return
+    for (let i = 0; i < index.length; i++) {
+        result.push(price[index[i]]);
     }
-
-    var ret = []
-    for (var i = 0; i < ia.length; i++) {
-        ret.push(stamps[ia[i]])
-    }
-    return ret
+    return result
 }
--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var S = require('./stamp.js')
 describe('S.sumUpStamps', function () {
     describe('[1, 2, 3, 5, 10], [0, 0, 1, 2, 4]', function () {
         it('should be 17', function () {
-            assert.equal(15, S.sumUpStamps([1, 2, 3, 5, 10], [0, 0, 1, 2, 4]));
+            assert.equal(17, S.sumUpStamps([1, 2, 3, 5, 10], [0, 0, 1, 2, 4]));
         });
     });
     describe('[1, 2, 5, 10, 20], [0, 1, 1, 3, 3]', function () {
@@ -18,7 +18,7 @@ describe('S.sumUpStamps', function () {
     });
     describe('[1], [0, 0]', function () {
         it('should be undefined', function () {
-            assert.equal('undefined', typeof(S.sumUpStamps([1], [0, 0])));
+            assert.equal(2, S.sumUpStamps([1], [0, 0]));
         });
     });
 });
@@ -26,17 +26,59 @@ describe('S.sumUpStamps', function () {
 describe('S.initIA', function () {
     describe('[1, 2, 3, 5], 23', function () {
         it('should be [3, 3, 3, 3, 3]', function () {
-            assert.equal([3, 3, 3, 3, 3], S.initIA([1, 2, 3, 5], 23));
+            a = [3, 3, 3, 3, 3];
+            assert.deepEqual(S.initIA([1, 2, 3, 5], 23), a);
         });
     });
     describe('[2, 10], 15', function () {
         it('should be [1, 1]', function () {
-            assert.equal([1, 1], S.initIA([2, 10], 15));
+            assert.deepEqual(S.initIA([2, 10], 15), [1, 1]);
         });
     });
     describe('[], 1', function () {
         it('should be undefined', function () {
-            assert.equal('undefined', typeof(S.initIA([], 1)));
+            assert.deepEqual(typeof (S.initIA([], 1)), 'undefined');
         });
     });
 });
+describe('S.nextIAstepDown', function () {
+    it('should be [4, 4, 4, 4, 3]', function () {
+        assert.deepEqual(S.nextIAstepDown([1, 3, 5, 6, 7], [4, 4, 4, 4, 4], 29), [4, 4, 4, 4, 3]);
+    });
+    it('should be [4, 4, 4, 4, 2]', function () {
+        assert.deepEqual(S.nextIAstepDown([1, 3, 5, 6, 7], [4, 4, 4, 4, 3], 29), [4, 4, 4, 4, 2]);
+    });
+    it('should be [4, 4, 4, 4, 1]', function () {
+        assert.deepEqual(S.nextIAstepDown([1, 3, 5, 6, 7], [4, 4, 4, 4, 2], 29), [4, 4, 4, 4, 1]);
+    });
+    it('should be [4, 4, 4, 4, 0]', function () {
+        assert.deepEqual(S.nextIAstepDown([1, 3, 5, 6, 7], [4, 4, 4, 4, 1], 29), [4, 4, 4, 4, 0]);
+    });
+    it('should be [4, 4, 4, 4, 0]索引值低於零情形的 迴圈中斷測試', function () {
+        assert.deepEqual(S.nextIAstepDown([1, 3, 5, 6, 7], [4, 4, 4, 4, 0], 29), [4, 4, 4, 4, 0]);
+    });
+    it('should be [3]', function () {
+        assert.deepEqual(S.nextIAstepDown([3, 4, 5, 6, 7], [4], 2), [3]);
+    });
+    it('should be [2]', function () {
+        assert.deepEqual(S.nextIAstepDown([3, 4, 5, 6, 7], [3], 2), [2]);
+    });
+    it('should be [1]', function () {
+        assert.deepEqual(S.nextIAstepDown([3, 4, 5, 6, 7], [2], 2), [1]);
+    });
+    it('should be [0]', function () {
+        assert.deepEqual(S.nextIAstepDown([3, 4, 5, 6, 7], [1], 2), [0]);
+    });
+    it('should be [0]索引值低於零情形的 迴圈中斷測試', function () {
+        assert.deepEqual(S.nextIAstepDown([3, 4, 5, 6, 7], [0], 2), [0]);
+    });
+});
+describe('S.findStamp', function () {
+    it('should be [7, 7, 7, 7, 1]', function (){
+        assert.deepEqual(S.findStamp([1, 3, 5, 6, 7], 29), [7, 7, 7, 7, 1]);
+    });
+    it('should be [7, 7, 7, 7, 1]永遠大於的情形(無相等解) 迴圈中斷測試', function (){
+        assert.deepEqual(S.findStamp([2, 3, 5, 6, 7], 29), [7, 7, 7, 7, 2]);
+    });
+});
+
